ff4c279f8d6c41893e652abeb87fecd86a393893
zeboudji <youssouf.zeboudji@insidegroup.fr>
2024-11-02 01:14:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+import os
+import webview
+import sys
 
-app = Flask(__name__)
+def resource_path(relative_path):
+    """Obtenir le chemin absolu pour les ressources, compatible avec PyInstaller."""
+    try:
+        # PyInstaller crée un dossier temporaire et stocke le chemin dans _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    return os.path.join(base_path, relative_path)
+
+def main():
+    # Chemin vers le fichier HTML
+    html_file = resource_path("index.html")
+    # Créer une fenêtre WebView
+    window = webview.create_window("Jeu du Dinosaure IA", html=html_file, width=1200, height=400)
+    # Lancer l'application
+    webview.start()
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    main()
